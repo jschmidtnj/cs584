71d5a2c6807f1265c5d006baddc4df173873ad95
Joshua <jschmidtnj@gmail.com>
2020-09-26 11:52:40 -0400

    assignment1: added more comments
    added comments and further explanations
    
    Signed-off-by: Joshua <jschmidtnj@gmail.com>

--- assignment1/package.sh
+++ assignment1/package.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-output=output.zip
+output=Assignment1_Schmidt_Joshua.zip
 
 rm -f "$output"
 
-zip -r "$output" *.md environment.yml src proof -x src/__pycache__/\* src/.ipynb_checkpoints/\*
+zip -r "$output" *.md environment.yml src proof data -x src/__pycache__/\* src/.ipynb_checkpoints/\* data/\*.txt
