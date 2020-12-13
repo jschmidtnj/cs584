f6fd03455cc5205410ba82e11ac7476e71867592
Joshua <jschmidtnj@gmail.com>
2020-12-12 23:30:18 -0500

    final: added roberta finally :)
    Signed-off-by: Joshua <jschmidtnj@gmail.com>

--- final_project/code/src/data.py
+++ final_project/code/src/data.py
@@ -13,6 +13,41 @@ from variables import raw_data_folder
 from tqdm import tqdm
 from sklearn.model_selection import train_test_split
 import numpy as np
+from transformers import DistilBertTokenizer, TFDistilBertModel, TFRobertaPreTrainedModel
+from tokenizers import BertWordPieceTokenizer
+
+
+def fast_encode(texts, tokenizer, chunk_size=256, maxlen=512):
+    """
+    Encoder for encoding the text into sequence of integers for BERT Input
+    """
+    tokenizer.enable_truncation(max_length=maxlen)
+    tokenizer.enable_padding(max_length=maxlen)
+    all_ids = []
+
+    for i in tqdm(range(0, len(texts), chunk_size)):
+        text_chunk = texts[i:i+chunk_size].tolist()
+        encs = tokenizer.encode_batch(text_chunk)
+        all_ids.extend([enc.ids for enc in encs])
+
+    return np.array(all_ids)
+
+
+def build_model(transformer, max_len=512):
+    """
+    function for training the BERT model
+    """
+    input_word_ids = tf.keras.Input(
+        shape=(max_len,), dtype=tf.int32, name="input_word_ids")
+    sequence_output = transformer(input_word_ids)[0]
+    cls_token = sequence_output[:, 0, :]
+    out = tf.keras.layers.Dense(1, activation='sigmoid')(cls_token)
+
+    model = tf.keras.Model(inputs=input_word_ids, outputs=out)
+    model.compile(tf.keras.optimizers.Adam(lr=1e-5), loss='binary_crossentropy',
+                  metrics=['accuracy'])
+
+    return model
 
 
 def read_data(strategy) -> pd.DataFrame:
@@ -165,6 +200,112 @@ def read_data(strategy) -> pd.DataFrame:
     logger.info(f"AUC: {roc_auc(scores, yvalid):.2f}")
 
     # Attention models
+
+    # IMP DATA FOR CONFIG
+
+    AUTO = tf.data.experimental.AUTOTUNE
+
+    # Configuration
+    EPOCHS = 3
+    BATCH_SIZE = 16 * strategy.num_replicas_in_sync
+    MAX_LEN = 192
+
+    # First load the real tokenizer
+    tokenizer = DistilBertTokenizer.from_pretrained(
+        'distilbert-base-multilingual-cased')
+    # Save the loaded tokenizer locally
+    tokenizer.save_pretrained('.')
+    # Reload it with the huggingface tokenizers library
+    fast_tokenizer = BertWordPieceTokenizer('vocab.txt', lowercase=False)
+
+    train1 = pd.read_csv(file_path_relative(
+        f'{raw_data_folder}/jigsaw-toxic-comment-train.csv'))
+    valid = pd.read_csv(file_path_relative(
+        f'{raw_data_folder}/validation.csv'))
+
+    x_train = fast_encode(train1.comment_text.astype(str),
+                          fast_tokenizer, maxlen=MAX_LEN)
+    x_valid = fast_encode(valid.comment_text.astype(str),
+                          fast_tokenizer, maxlen=MAX_LEN)
+    x_test = fast_encode(test.content.astype(
+        str), fast_tokenizer, maxlen=MAX_LEN)
+
+    y_train = train1.toxic.values
+    y_valid = valid.toxic.values
+
+    train_dataset = (
+        tf.data.Dataset
+        .from_tensor_slices((x_train, y_train))
+        .repeat()
+        .shuffle(2048)
+        .batch(BATCH_SIZE)
+        .prefetch(AUTO)
+    )
+
+    valid_dataset = (
+        tf.data.Dataset
+        .from_tensor_slices((x_valid, y_valid))
+        .batch(BATCH_SIZE)
+        .cache()
+        .prefetch(AUTO)
+    )
+
+    test_dataset = (
+        tf.data.Dataset
+        .from_tensor_slices(x_test)
+        .batch(BATCH_SIZE)
+    )
+
+    with strategy.scope():
+        transformer_layer = (
+            TFDistilBertModel
+            .from_pretrained('distilbert-base-multilingual-cased')
+        )
+        model = build_model(transformer_layer, max_len=MAX_LEN)
+    model.summary()
+
+    n_steps = x_train.shape[0] // BATCH_SIZE
+    train_history = model.fit(
+        train_dataset,
+        steps_per_epoch=n_steps,
+        validation_data=valid_dataset,
+        epochs=EPOCHS
+    )
+
+    n_steps = x_valid.shape[0] // BATCH_SIZE
+    train_history_2 = model.fit(
+        valid_dataset.repeat(),
+        steps_per_epoch=n_steps,
+        epochs=EPOCHS*2
+    )
+
+    scores = model.predict(test_dataset, verbose=1)
+    logger.info(f"AUC: {roc_auc(scores, yvalid):.2f}")
+
+    # Roberta
+
+    with strategy.scope():
+        transformer_layer = TFRobertaPreTrainedModel()
+        model = build_model(transformer_layer, max_len=MAX_LEN)
+    model.summary()
+
+    n_steps = x_train.shape[0] // BATCH_SIZE
+    train_history = model.fit(
+        train_dataset,
+        steps_per_epoch=n_steps,
+        validation_data=valid_dataset,
+        epochs=EPOCHS
+    )
+
+    n_steps = x_valid.shape[0] // BATCH_SIZE
+    train_history_2 = model.fit(
+        valid_dataset.repeat(),
+        steps_per_epoch=n_steps,
+        epochs=EPOCHS
+    )
+
+    scores = model.predict(test_dataset, verbose=1)
+    logger.info(f"AUC: {roc_auc(scores, yvalid):.2f}")
 
 
 if __name__ == '__main__':
